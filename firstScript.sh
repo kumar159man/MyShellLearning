1121c1bd96a61595647972199c23fc884d258fec
kumar159man <45995987+kumar159man@users.noreply.github.com>
2020-02-11 17:20:02 +0530

    Create firstScript.sh

---
+++ firstScript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# For running this script file you need to ensure +x rights are given if not use chmod to give execute permission
+# Command to run is ./{fileName}
+# Hash is used for writing comments
+# first line specifies which shell interpretor to be used
+
+# To find out what is currently active shell run below command
+
+ps | grep $$
+
+# For inserting new line echo command is used
+echo
+
+# To find out find out the full path of the shell interpreter
+which bash
+
+# Print something on terminal
+
+echo HelloWorld 
+
+echo
+echo "Hello Shell"
+echo
+echo 'Hellllllllllloooooooooooo'
